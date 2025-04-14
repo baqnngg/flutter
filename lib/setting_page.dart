a4966d20bee2af3c1fb8aa1c9c64e49db2fca642
baqnngg <bbqdnrmas@gmail.com>
2025-04-14 10:06:56 +0900

    4_14

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello/drawer.dart';
+import 'package:first/drawer.dart';
 
 class SettingPage extends StatelessWidget {
   const SettingPage({super.key});
